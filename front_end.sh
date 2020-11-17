232a75063d48d63b654658563fcc2170759d7238
Pedro Ángel (Bama) <60348762+vaeruiz@users.noreply.github.com>
2020-11-17 17:54:29 +0100

    Eliminado repetido

--- front_end.sh
+++ front_end.sh
@@ -34,7 +34,6 @@ mv adminer-4.7.7-mysql.php index.php
 # Instar GoAcces
 echo "deb http://deb.goaccess.io/ $(lsb_release -cs) main" | sudo tee -a /etc/apt/sources.list.d/goaccess.list
 wget -O - https://deb.goaccess.io/gnugpg.key | sudo apt-key add -
-apt update
 apt install goaccess -y
 
 # Directorio de consulta de estadísticas
